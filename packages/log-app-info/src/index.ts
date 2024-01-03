419deac6ae7f11e08f68884ef0a5c31b4d27690e
thjxs <tanhongjian@outlook.com>
2024-01-03 17:37:38 +0800

    Make log style configurable

--- packages/log-app-info/src/index.ts
+++ packages/log-app-info/src/index.ts
@@ -2,27 +2,43 @@ import { execSync } from 'child_process';
 
 const THEME = '#FF6AC1';
 
-function createLogStyle(color: string) {
-  return `color: ${color};font-size: 16px;`;
+const defaultStyle = {
+  color: THEME,
+  fontSize: '16px',
+};
+
+function toDashCase(str: string) {
+  return str.replace(/([A-Z])/g, '-$1').toLowerCase();
 }
 
-function toConsoleMessage(message: string, color: string) {
-  return `console.log('%c${message}','${createLogStyle(color)}');`;
+function createLogStyle(style: Record<string, string>) {
+  let keys = Object.keys(style);
+  return keys.map((key) => `${toDashCase(key)}: ${style[key]}`).join(';');
+}
+
+function toConsoleMessage(message: string, style: Record<string, string>) {
+  return `console.log('%c${message}','${createLogStyle(style)}');`;
 }
 
 function getGitCommitHash() {
-  return execSync('git rev-parse HEAD', { encoding: 'utf-8' });
+  try {
+    return execSync('git rev-parse HEAD', { encoding: 'utf-8' });
+  } catch (error) {
+    return 'Invalid commit hash';
+  }
 }
 
-export default function main(version: string) {
+export default function main(version: string, style?: Record<string, string>) {
   const commit = getGitCommitHash();
   const commitHash = commit.toString().trim();
   const buildDate = new Date().toLocaleString();
 
+  const styleConfig = { ...defaultStyle, ...style };
+
   const script = `
-    ${toConsoleMessage(`${version}`, THEME)}
-    ${toConsoleMessage(`${buildDate}`, THEME)}
-    ${toConsoleMessage(`${commitHash}`, THEME)}
+    ${toConsoleMessage(`${version}`, styleConfig)}
+    ${toConsoleMessage(`${buildDate}`, styleConfig)}
+    ${toConsoleMessage(`${commitHash}`, styleConfig)}
   `;
   return script;
 }
